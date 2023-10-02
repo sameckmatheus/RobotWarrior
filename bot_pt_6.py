a4959e2769978c1a8252297b5985531e73552c41
sameckmatheus <sameckmatheuspro@gmail.com>
2023-10-02 10:33:57 -0300

    Update: Change Robot Warrior Part 6

---
+++ bot_pt_6.py
@@ -0,0 +1,37 @@
+def play():
+    playing = True
+    print("Welcome to the game!")
+    print("Datas for player 1:")
+    robot_one = build_robot()
+    print("Datas for player 2:")
+    robot_two = build_robot()
+
+    current_robot = robot_one
+    enemy_robot = robot_two
+    rount = 0
+
+    while playing:
+        if rount % 2 == 0:
+            current_robot = robot_one
+            enemy_robot = robot_two
+        else:
+            current_robot = robot_two
+            enemy_robot = robot_one
+        current_robot.print_status()
+        print("What part should I use to attack?:")
+        part_to_use = input("Choose a number part: ")
+        part_to_use = int(part_to_use)
+
+        enemy_robot.print_status()
+        print("Which part of the enemy should we attack?")
+        part_to_attack = input("Choose a enemy number part to attack: ")
+        part_to_attack = int(part_to_attack)
+
+        current_robot.attack(enemy_robot, part_to_use, part_to_attack)
+        rount += 1
+        if not enemy_robot.is_on() or enemy_robot.is_there_available_part() == False:
+            playing = False
+            print("Congratulations, you won")
+
+
+play()
