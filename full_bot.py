c592c9d27eca3a49c1e7f16acab6cd2d6c345356
sameckmatheus <sameckmatheus>
2023-10-04 08:15:20 -0300

    Update: Reformat Structure Code For New Methods

--- full_bot.py
+++ full_bot.py
@@ -48,6 +48,7 @@ colors = {
     "Yellow": '\x1b[93m',
 }
 
+
 class Part:
 
     def __init__(self, name: str, attack_level=0, defense_level=0, energy_consumption=0):
@@ -73,6 +74,65 @@ class Part:
 
     def is_available(self):
         return self.defense_level <= 0
+
+
+def choose_color():
+    available_colors = colors
+    print("Available colors:")
+    for key, value in available_colors.items():
+        print(value, key)
+    print(colors["White"])
+    chosen_color = input("Choose a color: ")
+    color_code = available_colors[chosen_color]
+    return color_code
+
+
+def build_robot():
+    robot_name = input("Robot name: ")
+    color_code = choose_color()
+    robot = Robot(robot_name, color_code)
+    robot.print_status()
+    return robot
+
+
+def play():
+    """
+
+    :rtype: object
+    """
+    playing = True
+    print("Welcome to the game!")
+    print("Datas for player 1:")
+    robot_one = build_robot()
+    print("Datas for player 2:")
+    robot_two = build_robot()
+
+    current_robot = robot_one
+    enemy_robot = robot_two
+    rounds = 0
+
+    while playing:
+        if rounds % 2 == 0:
+            current_robot = robot_one
+            enemy_robot = robot_two
+        else:
+            current_robot = robot_two
+            enemy_robot = robot_one
+        current_robot.print_status()
+        print("What part should I use to attack?:")
+        part_to_use = input("Choose a number part: ")
+        part_to_use = int(part_to_use)
+
+        enemy_robot.print_status()
+        print("Which part of the enemy should we attack?")
+        part_to_attack = input("Choose a enemy number part to attack: ")
+        part_to_attack = int(part_to_attack)
+
+        current_robot.attack(enemy_robot, part_to_use, part_to_attack)
+        rounds += 1
+        if not enemy_robot.is_on() or enemy_robot.is_there_available_part():
+            playing = False
+            print("Congratulations, you won")
 
 
 class Robot:
@@ -123,60 +183,4 @@ class Robot:
         enemy_robot.parts[part_to_attack].reduce_edefense(self.parts[part_to_use].attack_level)
         self.energy -= self.parts[part_to_use].energy_consumption
 
-
-def build_robot():
-    robot_name = input("Robot name: ")
-    color_code = choose_color()
-    robot = Robot(robot_name, color_code)
-    robot.print_status()
-    return robot
-
-
-def choose_color():
-    available_colors = colors
-    print("Available colors:")
-    for key, value in available_colors.items():
-        print(value, key)
-    print(colors["White"])
-    chosen_color = input("Choose a color: ")
-    color_code = available_colors[chosen_color]
-    return color_code
-
-
-def play():
-    playing = True
-    print("Welcome to the game!")
-    print("Datas for player 1:")
-    robot_one = build_robot()
-    print("Datas for player 2:")
-    robot_two = build_robot()
-
-    current_robot = robot_one
-    enemy_robot = robot_two
-    rounds = 0
-
-    while playing:
-        if rounds % 2 == 0:
-            current_robot = robot_one
-            enemy_robot = robot_two
-        else:
-            current_robot = robot_two
-            enemy_robot = robot_one
-        current_robot.print_status()
-        print("What part should I use to attack?:")
-        part_to_use = input("Choose a number part: ")
-        part_to_use = int(part_to_use)
-
-        enemy_robot.print_status()
-        print("Which part of the enemy should we attack?")
-        part_to_attack = input("Choose a enemy number part to attack: ")
-        part_to_attack = int(part_to_attack)
-
-        current_robot.attack(enemy_robot, part_to_use, part_to_attack)
-        rounds += 1
-        if not enemy_robot.is_on() or enemy_robot.is_there_available_part() == False:
-            playing = False
-            print("Congratulations, you won")
-
-
-play()
+    play()
